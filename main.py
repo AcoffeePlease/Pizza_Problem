66bf494337944034f06b801af7a100af3338b166
SudatiSimone <39488019+SudatiSimone@users.noreply.github.com>
2020-10-27 11:32:46 +0100

    update

--- main.py
+++ main.py
@@ -1,35 +1,40 @@
-import numpy
+import numpy as np
 
-def main():
-    # TODO: Input da acquisire da file
+"""
+  Leggere file input di questo formato
+  3 5 1 6
+  TTTTT
+  TMMMT
+  TTTTT
+"""
+def read_file(filename):
+    lines = open(filename).readlines()
+    R, C, L, H = [int(val) for val in lines[0].split()]
+    pizza = np.array(
+        [list(map(lambda item: 1 if item == 'T' else 0, row.strip()))
+         for row in lines[1:]])
+    return R, C, L, H, pizza
 
-    # Leggere file input
-    in_file = open("input.txt", "r")
-    text = in_file.readline()
-    R = int(text[0])  # 3 rows
-    C = int(text[1])  # 5 columns
-    L = int(text[2])  # min 1 of each ingredient per slice
-    H = int(text[3])  # max 6 cells per slice
-
-    index = 3
-    A = np.ones((3,2))
-    for column in range(C):
-        for row in range(R):
-            A[row, column] = int(text[index])
-            index = index + 1
-
-
-    in_file.close()
-
-    #A = [["T", "T", "T", "T", "T"],
-    #    ["T", "M", "M", "M", "T"],
-    #    ["T", "T", "T", "T", "T"]]
-
-    # Scrivere file output
+"""
+  Scrivere file output con questo formato
+  3
+  0 0 2 1
+  0 2 2 2
+  0 3 2 4
+"""
+#  TODO: write file output
+def write_file(filename):
     out_file = open("output.txt", "w")
     x = 52
     out_file.write(str(x))
     out_file.close()
+
+def main():
+    R, C, L, H, pizza = read_file('input.txt')
+    print("R={} , C={} , L={} , H={} , \npizza=\n{} ".format(R, C, L, H, pizza))
+
+
+
 
 if __name__ == '__main__':
     main()
